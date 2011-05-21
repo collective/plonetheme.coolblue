fe9f295c7214ef7989c7b013e9d0f59a36d26136
Alex Clark <aclark@aclark.net>
2011-05-21 14:59:42 -0400

    Add resources

--- setup.py
+++ setup.py
@@ -4,8 +4,12 @@ setup(
     name='plonetheme.coolblue',
     packages=find_packages(),
     namespace_packages=['plonetheme'],
+    install_requires=[
+        'plone.app.theming',
+        'z3c.jbot',
+    ],
     entry_points="""
     [z3c.autoinclude.plugin]
     target = plone
-    """
+    """,
 )
