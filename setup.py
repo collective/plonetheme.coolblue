4de1cc5ebd568ce332e65f842f1f77138b5970aa
Alex Clark <aclark@aclark.net>
2011-05-22 09:43:55 -0400

    Add to setup, theming tweaks

--- setup.py
+++ setup.py
@@ -2,14 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
     name='plonetheme.coolblue',
+    version='0.1.0',
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
+    url='https://github.com/aclark4life/plonetheme.coolblue',
     packages=find_packages(),
-    namespace_packages=['plonetheme'],
+    namespace_packages=[
+        'plonetheme'
+    ],
     install_requires=[
         'plone.app.theming',
         'z3c.jbot',
     ],
-    entry_points="""
-    [z3c.autoinclude.plugin]
-    target = plone
-    """,
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+    ],
+    entry_points={
+        'z3c.autoinclude.plugin': 'target = plone',
+    },
 )
